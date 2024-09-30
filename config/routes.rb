38f997bc96128867aa1216716e3cb64af73e6b77
Tia Widi <tia.septianawidi@gmail.com>
2024-09-30 20:05:00 +0700

    Init devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "books#index"
+
+  devise_for :users
   
   resources :users
   resources :authors
