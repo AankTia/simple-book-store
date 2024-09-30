0529f5c27586b0fac86297c23ba6acacdc28d78c
Tia Widi <tia.septianawidi@gmail.com>
2024-09-30 17:58:57 +0700

    Add publication_year atttribute

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,10 @@ class Book
 
   field :title, type: String
   field :isbn, type: String
+  field :publication_year, type: Integer
 
   validates :title, presence: true
+  validates :publication_year, presence: true
 
   belongs_to :author, class_name: 'Author'
 end
